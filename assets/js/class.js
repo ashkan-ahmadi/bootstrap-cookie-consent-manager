d316f5999b200eb8dbd3f443396d3fe112e1dcaa
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2025-04-22 15:15:01 +0200

    Check if Bootstrap exists on initialization

--- assets/js/class.js
+++ assets/js/class.js
@@ -93,4 +93,23 @@ export default class cookieConsent {
 
     return configs
   }
+
+  // Other Functions
+
+  doesBootstrapExist() {
+    if (!bootstrap) {
+      console.error('BOOTSTRAP COOKIE CONSENT MANAGER: Bootstrap JS is not found. Make sure Bootstrap JS is loaded BEFORE loading this script. For more information, visit https://github.com/ashkan-ahmadi/bootstrap-cookie-consent-manager')
+      return false
+    }
+    return true
+  }
+
+  // INITIALIZE
+
+  initialize() {
+    // check if Bootstrap exists before anything else
+    if (!this.doesBootstrapExist()) {
+      return
+    }
+  }
 }
