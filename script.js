b34674526f7809a8a14fe7581495489166251699
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2025-04-22 14:59:34 +0200

    Update script

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ const consents = [
   {
     id: 'necessary',
     title: 'Necessary',
+    enabled: true,
     required: true,
     onByDefault: true,
     description: 'These cookies are necessary for the website to function properly and <b>cannot be switched off</b>. They help with things like logging in and setting your privacy preferences.</p>',
@@ -22,6 +23,7 @@ const consents = [
   {
     id: 'analytics',
     title: 'Analytics',
+    enabled: true,
     required: true,
     onByDefault: true,
     description: 'These cookies help us improve the site by tracking which pages are most popular and how visitors move around the site.',
@@ -29,6 +31,7 @@ const consents = [
   {
     id: 'advertising',
     title: 'Advertising',
+    enabled: true,
     required: false,
     onByDefault: true,
     description: 'These cookies provide extra features and personalization to improve your experience. They may be set by us or by partners whose services we use.',
@@ -36,9 +39,16 @@ const consents = [
   {
     id: 'personalization',
     title: 'Personalization',
+    enabled: true,
     required: false,
     onByDefault: true,
     description: 'These cookies allow our platform to show related and personalized content to improve your experience such as personalized recommendations or customized content based on your browsing history.',
   },
 ]
 bootstrapCookieConsentManager(consents, myconfig)
+
+// import cookieConsent from './assets/js/class.js'
+
+// const f = new cookieConsent(consents, myconfig)
+
+// console.log(f.getConfigs())
