bd8a747c4044586ce6254567184ee4a9aacfa569
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2025-05-01 17:09:05 +0200

    Limit onReject function callback to non-required types only

--- assets/js/cookieConsentManager.js
+++ assets/js/cookieConsentManager.js
@@ -279,13 +279,17 @@ export default class cookieConsentManager {
           event: `cookie_consent_reject_${type?.id}`,
         })
 
-        // Verify the key 'onAccept' exists and it's a function
-        // If a callback function exists, we run it
-        // If the key exists but a non-function is passed, we show a warning on Console
-        if (type.onReject && typeof type.onReject === 'function') {
-          type.onReject()
-        } else if (type.onReject && typeof type.onReject !== 'function') {
-          console.warn(`Property onReject on cookie consent type with id "${type.id}" expected a function but received a ${typeof type.onReject}. Review and make sure you pass a function if you want a callback to run on accepting this cookie consent type.`)
+        // not firing onReject function on types that have required:true
+        // if it's required, it should not be rejected
+        if (type.required !== true) {
+          // Verify the key 'onAccept' exists and it's a function
+          // If a callback function exists, we run it
+          // If the key exists but a non-function is passed, we show a warning on Console
+          if (type.onReject && typeof type.onReject === 'function') {
+            type.onReject()
+          } else if (type.onReject && typeof type.onReject !== 'function') {
+            console.warn(`Property onReject on cookie consent type with id "${type.id}" expected a function but received a ${typeof type.onReject}. Review and make sure you pass a function if you want a callback to run on accepting this cookie consent type.`)
+          }
         }
       })
 
