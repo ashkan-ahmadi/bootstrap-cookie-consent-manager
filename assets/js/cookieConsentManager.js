102120f2375cf293638c1459ac8bf0db1eb79f03
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2025-06-04 23:45:29 +0200

    Update getConsentTypePrefix()

--- assets/js/cookieConsentManager.js
+++ assets/js/cookieConsentManager.js
@@ -91,7 +91,8 @@ class cookieConsentManager {
   // +-------------------------------------+
 
   init() {
-    console.log(this.getPrefix())
+    // TODO: delete this
+    console.log(this.getConsentTypePrefix())
     // check if Bootstrap exists before anything else
     if (!this.bootstrapExists()) {
       console.error('BOOTSTRAP COOKIE CONSENT MANAGER: Bootstrap JS is not found. Make sure Bootstrap JS is loaded BEFORE loading this script. For more information, visit https://github.com/ashkan-ahmadi/bootstrap-cookie-consent-manager')
@@ -137,11 +138,21 @@ class cookieConsentManager {
       console.warn(`The 'prefix' value is not found or it's empty. Make sure you pass a prefix value (or remove to set the default value)`)
     }
 
-    return `${prefix}_`
+    return prefix + '_'
   }
 
   getConsentTypePrefix() {
-    return this.CONSENT_TYPE_PREFIX
+    const prefix = this.getPrefix()
+
+    const configs = this.getConfigs()
+
+    const { consentTypePrefix } = configs || {}
+
+    if (!consentTypePrefix) {
+      console.warn(`The 'prefix' value is not found or it's empty. Make sure you pass a prefix value (or remove to set the default value)`)
+    }
+
+    return prefix + consentTypePrefix + '_'
   }
 
   getConsentSetName() {
