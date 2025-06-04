065221f7c8e1ecac10053bfd5c11dc4081ca6c80
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2025-06-04 23:56:59 +0200

    Update getConsentSetPositiveValue

--- assets/js/cookieConsentManager.js
+++ assets/js/cookieConsentManager.js
@@ -94,7 +94,7 @@ class cookieConsentManager {
 
   init() {
     // TODO: delete this
-    console.log(this.getConsentSetValue())
+    console.log(this.getConsentSetPositiveValue())
     // check if Bootstrap exists before anything else
     if (!this.bootstrapExists()) {
       console.error('BOOTSTRAP COOKIE CONSENT MANAGER: Bootstrap JS is not found. Make sure Bootstrap JS is loaded BEFORE loading this script. For more information, visit https://github.com/ashkan-ahmadi/bootstrap-cookie-consent-manager')
@@ -184,7 +184,15 @@ class cookieConsentManager {
   }
 
   getConsentSetPositiveValue() {
-    return this.SET_POSITIVE_VALUE
+    const configs = this.getConfigs()
+
+    const { setPositiveValue } = configs || {}
+
+    if (!setPositiveValue) {
+      console.warn(`The 'setPositiveValue' value is not found or it's empty. Make sure you pass a setPositiveValue value (or remove to set the default value)`)
+    }
+
+    return setPositiveValue
   }
 
   getConsentSetNegativeValue() {
