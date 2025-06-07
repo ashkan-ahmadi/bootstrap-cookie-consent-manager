f01cdfab9fde4d031661179ab79fc5fbfa251476
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2025-06-07 16:15:39 +0200

    Simplify onReject callback on Reject All

--- assets/js/cookieConsentManager.js
+++ assets/js/cookieConsentManager.js
@@ -363,13 +363,12 @@ class cookieConsentManager {
         // not firing onReject function on types that have required:true
         // if it's required, it should not be rejected
         if (type.required !== true) {
-          // Verify the key 'onReject' exists and it's a function
-          // If a callback function exists, we run it
-          // If the key exists but a non-function is passed, we show a warning on Console
-          if (type.onReject && typeof type.onReject === 'function') {
-            type.onReject()
-          } else if (type.onReject && typeof type.onReject !== 'function') {
-            console.warn(`Property onReject on cookie consent type with id "${type.id}" expected a function but received a ${typeof type.onReject}. Review and make sure you pass a function if you want a callback to run on accepting this cookie consent type.`)
+          if (type.onReject) {
+            if (typeof type.onReject === 'function') {
+              type.onReject()
+            } else {
+              console.warn(`Property onReject on cookie consent type with id "${type.id}" expected a function but received a ${typeof type.onReject}. Review and make sure you pass a function if you want a callback to run on accepting this cookie consent type.`)
+            }
           }
         }
       })
